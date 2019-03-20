f7fc5a4ea8ddd4c33e7728de28a95b1dc0199bfe
abbiyanaila <abbiyanaila@gmail.com>
2019-03-20 13:23:55 +0800

    fix: add information in every sintaks

--- predict.py
+++ predict.py
@@ -1,8 +1,9 @@
+"""import library that will you use"""
 import numpy as np
 import pandas as pd
 import argparse
 
-
+"""command-line parsing in the Python standard library"""
 parser = argparse.ArgumentParser(description='prediction boston housing')
 parser.add_argument('--LSTAT', type=float, help='% lower status of the population')
 parser.add_argument('--RM', type=float, help='average number of rooms per dwelling')
@@ -10,7 +11,8 @@ parser.add_argument('--PTRATIO', type=float, help='pupil-teacher ratio by town')
 parser.add_argument('--load', default=None, help='you can load the file from the folder')
 args = parser.parse_args()
 
-
+"""make function predict_boston for declaration features array and load the pkl filename from
+the folder"""
 def predict_boston(LSTAT, RM, PTRATIO):
     test = np.array([[LSTAT, RM, PTRATIO]])
     if args.load == None:
@@ -21,7 +23,9 @@ def predict_boston(LSTAT, RM, PTRATIO):
         pred = clf.predict(test)
     return pred[0]
 
+"""build variable which evaluates to the name of current module"""
 if __name__ == '__main__':
+    """make predict with call the arguments and print it out"""
     out = predict_boston(args.LSTAT, args.RM, args.PTRATIO)
     print(f'The prediction of price is: ${out}')
 
